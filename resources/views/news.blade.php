afeedbfb627408dbfa30b27206d050dff4117001
irmaulidia <irmamaulidiaaa@gmail.com>
2022-03-01 23:55:21 +0700

    View

---
+++ resources/views/news.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Berita {{id}}</h2>
+    </div>
+@endsection
+
---
+++ resources/views/program.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Halaman Daftar Program</h2>
+    <ul class="list-group">
+        @foreach($product as $item)
+        <li class="list-group-item"><a href="/product">{{$item}}</a></li>
+        @endforeach
+    </ul>
+</div>
+@endsection
